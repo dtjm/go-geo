cad2164d295ef3e75f882e71219f3e9595bb0cb0
Sam Nguyen <samxnguyen@gmail.com>
2011-06-24 15:37:27 -0700

    gofmt all files

--- geo.go
+++ geo.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-    EarthRadiusMi = 3959
-    EarthRadiusKm = 6371
+	EarthRadiusMi = 3959
+	EarthRadiusKm = 6371
 )
 
 // Calculate Haversine distance between two lat/lon points on Earth.
@@ -16,8 +16,8 @@ func Haversine(startLat, startLon, endLat, endLon float64) float64 {
 	dLat := endLat - startLat
 	dLon := endLon - startLon
 
-	a := math.Pow(math.Sin(dLat/2), 2) + math.Cos(startLat) * math.Cos(endLat) * math.Pow(math.Sin(dLon/2), 2)
-    c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
-    d := EarthRadiusKm * c
+	a := math.Pow(math.Sin(dLat/2), 2) + math.Cos(startLat)*math.Cos(endLat)*math.Pow(math.Sin(dLon/2), 2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	d := EarthRadiusKm * c
 	return d
 }
