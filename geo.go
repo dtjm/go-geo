6d408d6f65e39432571ccced3d1e84bd0327a267
mikequentel <mikequentel@yahoo.com>
2018-04-08 00:19:48 -0400

    Support for calculating distance using Vincenty formula

--- geo.go
+++ geo.go
@@ -1,6 +1,7 @@
 package geo
 
 import (
+	// "errors"
 	"math"
 )
 
@@ -20,4 +21,93 @@ func Haversine(startLat, startLon, endLat, endLon float64) float64 {
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 	d := EarthRadiusKm * c
 	return d
+}
+
+// Rounding function courtesy David Vaini
+// https://gist.github.com/DavidVaini/10308388
+func Round(val float64, roundOn float64, places int) (newVal float64) {
+	var round float64
+	pow := math.Pow(10, float64(places))
+	digit := pow * val
+	_, div := math.Modf(digit)
+	if div >= roundOn {
+		round = math.Ceil(digit)
+	} else {
+		round = math.Floor(digit)
+	}
+	newVal = round / pow
+	return
+}
+
+// DistVicenty is a re-write of the function LatLong.distVincenty
+// by Chris Veness ((c) 2002-2006 Chris Veness)
+// http://www.5thandpenn.com/GeoMaps/GMapsExamples/distanceComplete2.html
+// https://www.movable-type.co.uk/scripts/latlong-vincenty.html
+// based on DIRECT  AND INVERSE SOLUTIONS OF GEODESICS ON THE ELLIPSOID
+// WITH APPLICATION OF NESTED EQUATIONS, T. Vincenty, April 1975
+// https://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf
+// Calculate geodesic distance (in m) between two points specified by
+// latitude/longitude using Vincenty inverse formula for ellipsoids
+// Returns result in metres
+func DistVincenty(startLat, startLon, endLat, endLon float64) float64 {
+	// startLat := startLatDeg * (math.Pi / 180)
+	// startLon := startLonDeg * (math.Pi / 180)
+	// endLat := endLatDeg * (math.Pi / 180)
+	// endLon := endLonDeg * (math.Pi / 180)
+	a := 6378137.0
+	b := 6356752.3142
+	f := 1 / 298.257223563 // WGS-84 ellipsiod
+	L := endLon - startLon
+	U1 := math.Atan((1 - f) * math.Tan(startLat))
+	U2 := math.Atan((1 - f) * math.Tan(endLat))
+	sinU1 := math.Sin(U1)
+	cosU1 := math.Cos(U1)
+	sinU2 := math.Sin(U2)
+	cosU2 := math.Cos(U2)
+	lambda := L
+	lambdaP := 2 * math.Pi
+	iterLimit := 20
+	cosSqAlpha := 1.0
+	sinSigma := 1.0
+	cos2SigmaM := 1.0
+	cosSigma := 1.0
+	sigma := 1.0
+	// deltaLambda := lambda - lambdaP
+
+	for ((math.Abs(lambda - lambdaP)) > math.Pow10(-12)) && (iterLimit > 0) {
+		sinLambda := math.Sin(lambda)
+		cosLambda := math.Cos(lambda)
+		sinSigma = math.Sqrt((cosU2*sinLambda)*(cosU2*sinLambda) + (cosU1*sinU2-sinU1*cosU2*cosLambda)*(cosU1*sinU2-sinU1*cosU2*cosLambda))
+		if sinSigma == 0 {
+			// return 0, nil // co-incident points
+			return 0.0
+		}
+		cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda
+		sigma = math.Atan2(sinSigma, cosSigma)
+		sinAlpha := cosU1 * cosU2 * sinLambda / sinSigma
+		cosSqAlpha = 1 - sinAlpha*sinAlpha
+		cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha
+		if math.IsNaN(cos2SigmaM) {
+			cos2SigmaM = 0.0 // equatorial line: cosSqAlpha=0 (§6)
+		}
+		C := f / 16 * cosSqAlpha * (4 + f*(4-3*cosSqAlpha))
+		lambdaP = lambda
+		lambda = L + (1-C)*f*sinAlpha*(sigma+C*sinSigma*(cos2SigmaM+C*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)))
+		// deltaLambda = lambda - lambdaP
+		iterLimit = iterLimit - 1
+	}
+
+	if iterLimit == 0 {
+		//return -1.0, errors.New("formula failed to converge")
+		return -1.0
+	}
+
+	uSq := cosSqAlpha * (a*a - b*b) / (b * b)
+	A := 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)))
+	B := uSq / 1024 * (256 + uSq*(-128+uSq*(74-47*uSq)))
+	deltaSigma := B * sinSigma * (cos2SigmaM + B/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)-B/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)))
+	s := b * A * (sigma - deltaSigma)
+	s = s * math.Pow10(-3)
+	s = (Round(s, 0.5, 3))
+	return s
 }
--- geo_test.go
+++ geo_test.go
@@ -1,12 +1,13 @@
 package geo
 
 import (
-	"testing"
 	"math"
+	"testing"
 )
 
 const (
-	DistThreshold = 0.01
+	DistThreshold         = 0.01
+	DistThresholdVincenty = 0.5
 )
 
 func Deg2Rad(x float64) float64 {
@@ -46,5 +47,44 @@ func TestHaversine(t *testing.T) {
 func BenchmarkHaversine(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Haversine(1.1, 2.2, 3.3, 4.4)
+	}
+}
+
+func TestDistVincenty(t *testing.T) {
+	locations := make(map[string]([2]float64))
+	locations["Google HQ"] = [2]float64{37.422045, -122.084347}
+	locations["San Francisco"] = [2]float64{37.77493, -122.419416}
+	locations["Eiffel Tower"] = [2]float64{48.8582, 2.294407}
+	locations["Sydney Opera House"] = [2]float64{-33.856553, 151.214696}
+
+	testCases := make(map[string](map[string]float64))
+	for loc := range locations {
+		testCases[loc] = make(map[string]float64)
+	}
+
+	// http://www.wolframalpha.com/input/?i=distance+between+37.422045N,+122.084347W+and+37.77493N,+122.419416W
+	testCases["Google HQ"]["San Francisco"] = 49.09
+	// http://www.wolframalpha.com/input/?i=distance+between+37.422045N,+122.084347W+and+48.8582N,+2.294E
+	testCases["Google HQ"]["Eiffel Tower"] = 8990.0
+	// http://www.wolframalpha.com/input/?i=distance+between+37.422045N,+122.084347W+and+33.856553S,+151.214696E
+	testCases["Google HQ"]["Sydney Opera House"] = 11940.0
+
+	for startLoc, cases := range testCases {
+		start := locations[startLoc]
+		for endLoc, expected := range cases {
+			end := locations[endLoc]
+			dist := DistVincenty(Deg2Rad(start[0]), Deg2Rad(start[1]),
+				Deg2Rad(end[0]), Deg2Rad(end[1]))
+			if dist < expected-DistThresholdVincenty || dist > expected+DistThresholdVincenty {
+				t.Errorf("Distance from %s to %s should be ~%v km, got %v km",
+					startLoc, endLoc, expected, dist)
+			}
+		}
+	}
+}
+
+func BenchmarkDistVincenty(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		DistVincenty(1.1, 2.2, 3.3, 4.4)
 	}
 }
